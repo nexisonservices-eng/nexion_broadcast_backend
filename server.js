583736c1243137283ae92e83a7c4ee61d62d4348
Technovahub Career <technovahubcareer@gmail.com>
2026-01-26 19:46:20 +0530

    server.js edited

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ const allowedOrigins = [
   "http://127.0.0.1:60932",
   "http://localhost:53918",
   "http://localhost:60932",
+  "http://localhost:5174",
+  "http://localhost:5175",
+  "http://127.0.0.1:5174",
+  "http://127.0.0.1:5175",
 ].filter(Boolean);
 
 app.use(cors({
