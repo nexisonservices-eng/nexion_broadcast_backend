57ab3c73e1704d5b3acafe5266715a6c3e6299af
lirishatechnovahub <lirishatechnovahub@gmail.com>
2026-07-27 16:57:03 +0530

    fix: add authentication middleware to meta leads route

--- routes/metaLeadRoutes.js
+++ routes/metaLeadRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
+const auth = require('../middleware/auth');
 const { getMetaLeads } = require('../controllers/metaLeadController');
 
 const router = express.Router();
 
-router.get('/', getMetaLeads);
+router.get('/', auth, getMetaLeads);
 
 module.exports = router;
